53d2521caa0ecac2599c3833809b1ff3b56e8124
Joseph Grijalva <jig25052@gmail.com>
2022-05-16 20:58:17 -0500

    connected myApp.js to the mongodb database

--- myApp.js
+++ myApp.js
@@ -1,12 +1,7 @@
 require('dotenv').config();
+const mongoose = require('mongodb');
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const client = new MongoClient(process.env.MONGO_URI , { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 let Person;
 
