83714cceadfae3c3f56bb0735e2eb2f44d92bf92
Joseph Grijalva <jig25052@gmail.com>
2022-05-21 17:13:04 -0500

    Created the first model.

--- myApp.js
+++ myApp.js
@@ -1,9 +1,17 @@
 require('dotenv').config();
 const mongoose = require('mongodb');
+const Schema = mongoose.Schemea
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-let Person;
+const Person = mongoose.model("Person", personSchema);
+
+const pserSchema = new Schema({
+  name: {type: String, required: true},
+  age: Number, 
+  favoriteFoods: [String] 
+
+});
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
