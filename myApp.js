c49f8e41ec5e1741f93830ccee395a2b8ccd13e0
Joseph Grijalva <jig25052@gmail.com>
2022-05-25 20:51:04 -0500

    Made changes to the createAndSavePerson fucntion

--- myApp.js
+++ myApp.js
@@ -4,8 +4,6 @@ const Schema = mongoose.Schemea
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-const Person = mongoose.model("Person", personSchema);
-
 const personSchema = new Schema({
   name: {type: String, required: true},
   age: Number, 
@@ -13,8 +11,22 @@ const personSchema = new Schema({
 
 });
 
+const Person = mongoose.model("Person", personSchema);
+
 const createAndSavePerson = (done) => {
-  done(null /*, data*/);
+  
+  var josephGrijalva = new Person({
+    name: "Joseph",
+    age: 18 ,
+    favoriteFoods: [ "tacos", "sushi", "Thai tea"]
+  });
+
+  josephGrijalva.save(function(err, data) {
+    if (err) return console.error(err);
+    done(null, data)
+  });
+
+  
 };
 
 const createManyPeople = (arrayOfPeople, done) => {
